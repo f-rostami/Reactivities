013ce0e380137ece807dab70685b8f3a2a8203d3
Farbod <farbodkain@gmail.com>
2020-05-24 20:28:20 +0430

    Initial Commit

---
+++ API/Controllers/ValueController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly DataContext _ctx;
+
+        public ValueController(DataContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _ctx.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _ctx.Values.FindAsync(id);
+            return Ok(value);
+
+        }
+
+    }
+}
